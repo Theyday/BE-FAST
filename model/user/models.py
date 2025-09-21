9e5553112f4570fff6fbaf156519cb9e66617832
Junseo <monstazo911@gmail.com>
2025-09-21 16:39:55 +0900

    Add DB migration step and new coupon table migration script
    - Introduced a new step in the production workflow to run Alembic database migrations after deploying the application.
    - Created a new Alembic migration script to establish a coupon table and modify existing tables, enhancing the database schema for future features.
    - Updated the User model to include a nickname field, allowing for additional user customization.

--- model/user/models.py
+++ model/user/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, BigInteger, String
+from sqlalchemy import Column, BigInteger, Identity, String
 from sqlalchemy.orm import relationship
 
 from ..base_time_model import BaseTimeModel
@@ -6,11 +6,12 @@ from ..base_time_model import BaseTimeModel
 class User(BaseTimeModel):
     __tablename__ = "user_tb"
 
-    id = Column(BigInteger, primary_key=True, index=True)
+    id = Column(BigInteger, Identity(start=1, increment=1), primary_key=True)
     email = Column(String, unique=True, nullable=True)
     phone = Column(String, unique=True, nullable=True)
     name = Column(String, nullable=False)
     image = Column(String, nullable=True)
+    nickname = Column(String, nullable=True)
     
     routines = relationship("Routine", back_populates="user")
     participants = relationship("Participant", back_populates="user")
